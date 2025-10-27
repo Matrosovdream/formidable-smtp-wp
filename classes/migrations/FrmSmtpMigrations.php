6de1f7c5b140d41bda725978170e010ad6f479a2
Stan Matrosov <matrosovdream@gmail.com>
2025-10-27 19:19:46 +0100

    Migrations, new field original_log_id

--- classes/migrations/FrmSmtpMigrations.php
+++ classes/migrations/FrmSmtpMigrations.php
@@ -3,7 +3,7 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 class FrmSmtpMigrations {
 
-    public const DB_VERSION     = '1.0.4'; // bumped
+    public const DB_VERSION     = '1.0.5'; // bumped
     public const VERSION_OPTION = 'frm_smtp_db_version';
 
     public static function install(): void {
@@ -32,6 +32,7 @@ class FrmSmtpMigrations {
             attachments tinyint(3) unsigned NOT NULL DEFAULT 0,
             initiator_name varchar(255) NULL,
             initiator_file text NULL,
+            original_log_id bigint(20) unsigned NULL,
             PRIMARY KEY  (id),
             KEY idx_entry_id (entry_id),
             KEY idx_form_id (form_id),
